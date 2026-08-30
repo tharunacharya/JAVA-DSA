5a0a6dd6348a1701da97ba45b4dfe5d5590068a7
tharunacharya <137447940+tharunacharya@users.noreply.github.com>
2026-08-30 17:07:33 +0530

    Time: 1 ms (86.56%), Space: 44 MB (75.4%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,8 +1,7 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res=new ArrayList<>();
-        List<Integer> cur=new ArrayList<>();
-        sub(0,nums, cur, res);
+        sub(0,nums, new ArrayList<>(), res);
         return res;
         
     }
