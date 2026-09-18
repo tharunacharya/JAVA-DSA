56508f4c85b32d47e06f2b88e935112df071be5b
tharunacharya <137447940+tharunacharya@users.noreply.github.com>
2026-09-18 18:34:51 +0530

    Time: 1 ms (39.54%), Space: 45 MB (40.03%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,4 +1,19 @@
 class Solution {
+    //bottom up
+    public int minCostClimbingStairs(int[] cost) {
+        int n=cost.length;
+        int dp[]=new int[n+1];
+        dp[0]=0;
+        dp[1]=0;
+        for(int state=2;state<=n;state++){
+            int oneStep=cost[state-1]+dp[state-1];
+            int twoStep=cost[state-2]+dp[state-2];
+            dp[state]=Math.min(oneStep,twoStep);
+        }
+        return dp[n];
+    }
+
+
     // public int minCostClimbingStairs(int[] cost) {
     //     int res=rec(cost.length,cost);
     //     return res;
@@ -13,24 +28,24 @@ class Solution {
     // }
 
     //Top Down Approach
-    public int minCostClimbingStairs(int[] cost) {
-        int n=cost.length;
-        int dp[]=new int[n+1];
-        Arrays.fill(dp,-1);
-        int res=rec(n,cost,dp);
-        return res;
-    }
-    private int rec(int n,int []cost,int dp[]){
-        if(n<=1){
-            dp[n]=0;
-            return 0;
-        }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        int oneStep=cost[n-1]+rec(n-1,cost,dp);
-        int twoStep=cost[n-2]+rec(n-2,cost,dp);
-        dp[n]=Math.min(oneStep,twoStep);
-        return dp[n];
-    }
+    // public int minCostClimbingStairs(int[] cost) {
+    //     int n=cost.length;
+    //     int dp[]=new int[n+1];
+    //     Arrays.fill(dp,-1);
+    //     int res=rec(n,cost,dp);
+    //     return res;
+    // }
+    // private int rec(int n,int []cost,int dp[]){
+    //     if(n<=1){
+    //         dp[n]=0;
+    //         return 0;
+    //     }
+    //     if(dp[n]!=-1){
+    //         return dp[n];
+    //     }
+    //     int oneStep=cost[n-1]+rec(n-1,cost,dp);
+    //     int twoStep=cost[n-2]+rec(n-2,cost,dp);
+    //     dp[n]=Math.min(oneStep,twoStep);
+    //     return dp[n];
+    // }
 }
